8f9773fc31d6b42fb4359d570ec7ff4435c4f363
yanglee2421 <yanglee2421@gmail.com>
2025-02-05 13:24:26 +0800

    fix: preload

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
           resolve: {
             alias: {
               "@electron": fileURLToPath(
-                new URL("./electron", import.meta.url),
+                new URL("./electron", import.meta.url)
               ),
             },
           },
@@ -26,23 +26,31 @@ export default defineConfig({
         // Shortcut of `build.rollupOptions.input`.
         // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
         input: path.join(__dirname, "electron/preload.ts"),
+        vite: {
+          resolve: {
+            alias: {
+              "@electron": fileURLToPath(
+                new URL("./electron", import.meta.url)
+              ),
+            },
+          },
+        },
       },
       // Ployfill the Electron and Node.js API for Renderer process.
       // If you want use Node.js in Renderer process, the `nodeIntegration` needs to be enabled in the Main process.
       // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
-      renderer: process.env.NODE_ENV === "test"
-        // https://github.com/electron-vite/vite-plugin-electron-renderer/issues/78#issuecomment-2053600808
-        ? undefined
-        : {},
+      renderer:
+        process.env.NODE_ENV === "test"
+          ? // https://github.com/electron-vite/vite-plugin-electron-renderer/issues/78#issuecomment-2053600808
+            undefined
+          : {},
     }),
   ],
 
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
-      "@electron": fileURLToPath(
-        new URL("./electron", import.meta.url),
-      ),
+      "@electron": fileURLToPath(new URL("./electron", import.meta.url)),
     },
   },
 });
