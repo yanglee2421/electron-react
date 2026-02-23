5859afa39d08e3f608ab0d8c5bc7c40aa2455ea1
yang <yanglee2421@gmail.com>
2026-02-23 19:35:08 +0800

    style[electron-app]: Sort out code

--- packages/electron-app/src/main/lib/fs.ts
+++ packages/electron-app/src/main/lib/fs.ts
@@ -9,20 +9,20 @@ export const ls = async (basePath: string): Promise<string[]> => {
     return [basePath];
   }
 
-  if (stats.isDirectory()) {
-    const basenames = await fs.promises.readdir(basePath);
-    const limit = pLimit(1000);
-
-    const tasks = basenames.map((basename) => {
-      const subPath = path.resolve(basePath, basename);
-
-      return limit(() => ls(subPath));
-    });
-
-    const results = await Promise.all(tasks);
-
-    return results.flat();
+  if (!stats.isDirectory()) {
+    return [];
   }
 
-  return [];
+  const basenames = await fs.promises.readdir(basePath);
+  const limit = pLimit(1000);
+
+  const tasks = basenames.map((basename) => {
+    const subPath = path.resolve(basePath, basename);
+
+    return limit(() => ls(subPath));
+  });
+
+  const results = await Promise.all(tasks);
+
+  return results.flat();
 };
