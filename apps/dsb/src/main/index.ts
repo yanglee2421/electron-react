55fc9168001ad86a13201f380c3ed2242105490d
yanglee2421 <yanglee2421@gmail.com>
2026-06-22 17:30:43 +0800

    wip

--- apps/dsb/src/main/index.ts
+++ apps/dsb/src/main/index.ts
@@ -1,15 +1,48 @@
 import { app, BrowserWindow } from "electron";
+import { platform } from "node:os";
 import path from "node:path";
 import {
+  BehaviorSubject,
+  concat,
   defer,
+  filter,
   from,
   fromEventPattern,
+  ignoreElements,
   map,
+  mergeMap,
+  NEVER,
   of,
   partition,
-  switchMap,
+  startWith,
   take,
+  takeUntil,
+  tap,
+  using,
 } from "rxjs";
+
+const window$ = new BehaviorSubject<null | BrowserWindow>(null);
+const mainWindow$ = defer(() => {
+  // Create Browser Window and load page
+  const win = new BrowserWindow({
+    show: false,
+  });
+  win.menuBarVisible = false;
+
+  if (app.isPackaged) {
+    win.loadFile(path.resolve(__dirname, "../renderer/index.html"));
+  } else {
+    win.loadURL(process.env.DS_RENDERER_URL!);
+  }
+
+  //
+  const closed$ = fromEventPattern(
+    (f) => win.on("closed", f),
+    (f) => win.off("closed", f),
+  );
+
+  return NEVER.pipe(startWith(win), takeUntil(closed$));
+});
 
 const whenReady$ = from(app.whenReady());
 const willQuit$ = fromEventPattern(
@@ -31,38 +64,53 @@ const [primary$, duplicate$] = partition(
   (i) => i,
 );
 
+const resource$ = using(
+  () => {
+    console.log("subscribe");
+
+    return {
+      unsubscribe: () => {
+        console.log("unsubscribe");
+      },
+    };
+  },
+  () => NEVER.pipe(startWith(null), takeUntil(willQuit$)),
+);
+
 duplicate$.subscribe(() => {
   app.quit();
 });
 
-primary$.pipe(switchMap(() => whenReady$)).subscribe(() => {
-  const win = new BrowserWindow({
-    show: false,
-  });
-  win.menuBarVisible = false;
-
-  fromEventPattern(
-    (f) => win.on("ready-to-show", f),
-    (f) => win.off("ready-to-show", f),
-  )
-    .pipe(take(1))
-    .subscribe(() => {
-      win.show();
-    });
-
+concat(
+  primary$.pipe(ignoreElements()),
+  whenReady$.pipe(ignoreElements()),
+  resource$,
+).subscribe(() => {
   // win.webContents.on("did-finish-load", () => {
   // win.webContents.openDevTools();
   // });
 
-  if (app.isPackaged) {
-    win.loadFile(path.resolve(__dirname, "../renderer/index.html"));
-  } else {
-    win.loadURL(process.env.DS_RENDERER_URL!);
-  }
+  mainWindow$.subscribe(window$);
 });
 
-browserWindowCreated$.pipe(map(([, win]) => win)).subscribe();
-void {
-  windowAllClosed$,
-  willQuit$,
-};
+browserWindowCreated$
+  .pipe(
+    map(([, win]) => win),
+    mergeMap((win) => {
+      return fromEventPattern(
+        (f) => win.on("ready-to-show", f),
+        (f) => win.off("ready-to-show", f),
+      ).pipe(
+        take(1),
+        tap(() => win.show()),
+      );
+    }),
+  )
+  .subscribe();
+
+windowAllClosed$
+  .pipe(
+    filter(() => platform() !== "darwin"),
+    tap(() => app.quit()),
+  )
+  .subscribe();
